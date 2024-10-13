3e715fd9744bd31d61860618fbfd797fea5b2ae3
George Taylor <drumerboygt.gt@gmail.com>
2024-10-13 02:19:14 -0400

    Finished Project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -24,16 +25,51 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.send(JSON.stringify( books, null, 4));
+public_users.get('/', function (req, res) {
+
+  const promise = new Promise((resolve, reject) => {
+    if(books.length === 0){
+        reject("No books in the shop.");
+    } else {
+        resolve(true)
+    }
+  })
+
+  promise
+
+  .then(() => {
+    return res.send(JSON.stringify( books, null, 4));
+  })
+
+  .catch(err => {
+    console.log(err);
+  })
+
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn
-  return res.send(books[isbn]);
+
+  const promise = new Promise((resolve, reject) => {
+    if(books.length === 0){
+        reject("No books in the shop.");
+    } else {
+        resolve(true)
+    }
+  })
+
+  promise
+
+  .then(() => {
+    return res.send(books[isbn]);
+  })
+
+  .catch(err => {
+    console.log(err);
+  })
+  
  });
   
 // Get book details based on author
@@ -41,31 +77,59 @@ public_users.get('/author/:author',function (req, res) {
   //Write your code here
   let author = req.params.author
   author = author.replace("_", " ");
-  const bookValues = Object.values(books);
-  bookValues.forEach(ele => {
-    if(ele.author === author){
-        return res.send(ele);
-    }
-  });
 
-  return res.status(300).json({message: "No Match for Author"});
+  const promise = new Promise((resolve, reject) => {
+    if(books.length === 0){
+        reject("No books in the shop.");
+    } else {
+        resolve(true)
+    }
+  })
+
+  promise
+
+  .then(() => {
+    const bookValues = Object.values(books);
+    bookValues.forEach(ele => {
+        if(ele.author === author){   
+            return res.send(ele);
+        } 
+    })
+
+  })
+    .catch(err => {
+        return res.status(300).json({message: "No Match for Author"});
+  })
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
   let title = req.params.title
-  console.log(title);
   title = title.replaceAll("_", " ");
-  console.log(title);
-  const bookValues = Object.values(books);
-  bookValues.forEach(ele => {
-    if(ele.title === title){
-        return res.send(ele);
+  
+  const promise = new Promise((resolve, reject) => {
+    if(books.length === 0){
+        reject("No books in the shop.");
+    } else {
+        resolve(true)
     }
-  });
+  })
 
-  return res.status(300).json({message: "No Match for Title"});
+  promise
+
+  .then(() => {
+    const bookValues = Object.values(books);
+    bookValues.forEach(ele => {
+        if(ele.title === title){   
+            return res.send(ele);
+        } 
+    })
+
+  })
+    .catch(err => {
+        return res.status(300).json({message: "No Match for Title"});
+  })
 });
 
 //  Get book review
